4435891e11056edb19556a4cf625f033d487a315
JJohn <jjohn@jjohns-laptop.T-mobile.com>
2020-10-25 14:50:42 -0400

    Fixes for US 13 and 14

--- parse_gedcom/sprint2_user_stories.py
+++ parse_gedcom/sprint2_user_stories.py
@@ -3,8 +3,29 @@
 # US12 - Jenn
 
 # US13 - Justin
+#Justin John 
+#US 13 Child should be born before 9 months after death of father
+def US13_FatherDeath_Before_Child_Birth(fam):
+   day1 = fam.husbandObject.DeathDateObject
+   day2 = fam.child.birthDateObject
+   day3 = (((day2 - day1).days)
+   if day3 > 274:
+     fam.anomalies.append("Child was born after 9 months after death of father")
 
 # US14 - Justin
+# #Justin John 
+# #Mother should be less than 60 years older than her children and father should be less than 80 years older than his children 
+def US14_Parents_not_too_old(fam):
+  day1 = fam.wifeObject.birthDateObject
+  day2 = fam.child.birthDateObject
+  day3 = (((day2 - day1).days)/365)
+  day4 = fam.husbandObject.birthDateObject
+  day5 = fam.child.birthDateObject 
+  day6 = (((day5 - day4).days)/365) 
+  if day3 >= 60:
+    fam.anomalies.append("Mother is 60 years older than child/ren")
+  if day6 >= 80:
+    fam.anomalies.append("Father is 80 years older than child/ren")
 
 # US15 - Matt
 
