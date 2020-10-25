125f1b73f2e1da73d38c7a9d832be8cbf54f9a2c
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2020-10-25 06:00:57 -0400

    US19 implementation

--- parse_gedcom/sprint2_user_stories.py
+++ parse_gedcom/sprint2_user_stories.py
@@ -15,5 +15,8 @@
 # US18 - Liv
 
 # US19 - Angie
+def US19_no_marriages_to_descendants_anomaly(fam):
+    if (fam.husbandObject.childFamilyObject in fam.wifeObject.spouseFamilyObjects) or (fam.wifeObject.childFamilyObject in fam.husbandObject.spouseFamilyObjects):
+        fam.anomalies.append("Parents should not marry their children")
 
 # US20 - Angie
