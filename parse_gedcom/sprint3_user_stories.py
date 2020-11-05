0c81c6eae858e1ebd66a452b29022a9f49f7a077
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2020-11-05 01:18:23 -0500

    added exception handling
    Co-authored-by: Jennifer Lloyd <jlloyd2@stevens.edu>

--- parse_gedcom/sprint3_user_stories.py
+++ parse_gedcom/sprint3_user_stories.py
@@ -2,18 +2,23 @@
 # wife - get mom famc and dad family id
 #husband mom famc and dad famc and compare moms and 
 def US21_first_cousins_should_not_marry_anomaly(fam):
-    wifeMom = ""
-    wifeDad = ""
-    husbandMom = ""
-    husbandDad = ""
-    if fam.wifeObject.childFamilyObject and fam.wifeObject.childFamilyObject.wifeObject and fam.wifeObject.childFamilyObject.wifeObject.childFamilyObject:
+    wifeMom = wifeDad = husbandMom = husbandDad = ""
+    try:
         wifeMom = fam.wifeObject.childFamilyObject.wifeObject.childFamilyObject
-    if fam.wifeObject.childFamilyObject and fam.wifeObject.childFamilyObject.husbandObject and fam.wifeObject.childFamilyObject.husbandObject.childFamilyObject:
+    except AttributeError:
+        pass
+    try:
         wifeDad = fam.wifeObject.childFamilyObject.husbandObject.childFamilyObject
-    if fam.husbandObject.childFamilyObject and fam.husbandObject.childFamilyObject.wifeObject and fam.husbandObject.childFamilyObject.wifeObject.childFamilyObject:
+    except AttributeError:
+        pass
+    try:
         husbandMom = fam.husbandObject.childFamilyObject.wifeObject.childFamilyObject
-    if fam.husbandObject.childFamilyObject and fam.husbandObject.childFamilyObject.husbandObject and fam.husbandObject.childFamilyObject.husbandObject.childFamilyObject:
+    except AttributeError:
+        pass
+    try:
         husbandDad = fam.husbandObject.childFamilyObject.husbandObject.childFamilyObject
+    except AttributeError:
+        pass
     if wifeMom != "":
         if (husbandMom != "" and wifeMom == husbandMom) or (husbandDad != "" and wifeMom == husbandDad):
             fam.anomalies.append("First cousins should not marry")
