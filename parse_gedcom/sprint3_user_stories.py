596fe310ceaaeb1dde9854f3f63579790c8a2c20
JJohn <jjohn@ubuntu-laptop.T-mobile.com>
2020-11-07 16:35:47 -0500

    US 26 need ton of work. WIl fix later:
    Co-authored-by: Matthieu Dupuy <matt3327@gmail.com>

--- parse_gedcom/sprint3_user_stories.py
+++ parse_gedcom/sprint3_user_stories.py
@@ -12,8 +12,24 @@
 
 # US25 - Justin
 
+         
+
 
 # US26 - Justin
+# No more than one family with the same spouses by name and the same marriage date should appear in a GEDCOM file 
+def Unique_fam(fam):
+      for i in range(0,len(fam.individualObject)-1):
+        husbandname = fam.husbandObject[i].name
+        husbandbirth = fam.husbandObject[i].birthdate
+        wifename = fam.wifeObject[i].name
+        wifebirth= fam.wifeObject[i].birthdate
+        for j in range(i+1, len(indiv.individualObject)-1):
+            husbandname2 = fam.husbandObject[i].name
+            husbandbirth2 = fam.husbandObject[i].birthdate
+            wifename2 = fam.wifeObject[i].name
+            wifebirth=2 fam.wifeObject[i].birthdate
+            if(husband == husband2 or wife == wife2):  
+                indiv.anomalies.append("Same spouse with name and marriage date present")  
 
 
 # US27 - Angie
