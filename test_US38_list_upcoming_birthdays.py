1953e06865cb109fe60625801acd58ea6fdb9974
Olivia Powers <powersog@gmail.com>
2020-11-27 16:35:22 -0500

    updating file

--- test_US38_list_upcoming_birthdays.py
+++ test_US38_list_upcoming_birthdays.py
@@ -13,21 +13,18 @@ class Test(unittest.TestCase):
         person1 = self.person1
         person1.alive = True
         person1.birthDateObject = datetime(1999, 12, 25)
-        person1.age = 20
         self.assertEqual(US38_list_upcoming_birthdays(self.individuals), [person1])
 
     def test_upcoming_bday_dead(self):
         person1 = self.person1
         person1.alive = False
         person1.birthDateObject = datetime(1999, 12, 25)
-        person1.age = 20
         self.assertEqual(US38_list_upcoming_birthdays(self.individuals), [])
     
     def test_not_upcoming_bday_alive(self):
         person1 = self.person1
         person1.alive = True
         person1.birthDateObject = datetime(1999, 8, 30)
-        person1.age = 21
         self.assertEqual(US38_list_upcoming_birthdays(self.individuals), [])
 
 if __name__ == "__main__":
